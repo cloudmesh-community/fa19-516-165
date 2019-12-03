321ad6d573c6657922cfe864777c5b7f813a062d
zwang <zhi.neo.wang@gmail.com>
2019-12-03 16:17:54 -0500

    writing report and update code

--- project/cloudmesh_in_R.R
+++ project/cloudmesh_in_R.R
@@ -1,29 +1,17 @@
 # load  reticulate in R
 
-# have to indidate the system variable first in order to use it later
+# have to indidate the system variable first
 Sys.setenv(RETICULATE_PYTHON = "/Users/zwang/ENV3/bin/python")
 
 # use the reticulate library
 library(reticulate)
-
-# activate the Python environment
-use_python("/Users/zwang/ENV3/bin/python", required = TRUE)
+library(rcms)
 
 # double check the python config to enasure
 py_config()
 
-# import cloudmesh
-cloudmesh <- import("cloudmesh")
-
-# call cms function
-cloudmesh$cloud$Shell$cms("help")
-
-# define cms function
-cms <-function (command){
-  cloudmesh <- import("cloudmesh")
-  cloudmesh$cloud$Shell$cms(command)
-}
-
 # test two simple examples
+
 cms("help")
-cms("config")
+
+cms("version")
